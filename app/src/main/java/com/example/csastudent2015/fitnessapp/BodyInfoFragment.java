cdb90369f1e576115964be5a90ade5992535cb3b
330610 <jerry88tan@sbcglobal.net>
2016-01-12 09:36:47 -0800

    hi

---
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/BodyInfoFragment.java
@@ -0,0 +1,9 @@
+package com.example.csastudent2015.fitnessapp;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by csastudent2015 on 1/12/16.
+ */
+public class BodyInfoFragment extends Fragment{
+}
