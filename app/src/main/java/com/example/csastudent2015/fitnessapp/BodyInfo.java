fdf3fa7a517bc1ab0afa5dbbf8b935ce043f18e4
330610 <jerry88tan@sbcglobal.net>
2016-01-14 09:34:52 -0800

    hu

--- app/src/main/java/com/example/csastudent2015/fitnessapp/BodyInfo.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/BodyInfo.java
@@ -4,4 +4,28 @@ package com.example.csastudent2015.fitnessapp;
  * Created by csastudent2015 on 1/12/16.
  */
 public class BodyInfo {
+    private double weight;
+    private double height;
+    public double getHeight()
+    {
+        return height;
+    }
+   public double getWeight()
+   {
+       return weight;
+   }
+
+
+public double getBmi(){
+    weight = weight * 0.45;
+    height = height * 0.025;
+    height = Math.pow(height, 2);
+    return weight/height ;
+
 }
+
+
+}
+
+
+
