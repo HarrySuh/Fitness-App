eb8e65a4b8bc371d1a11309d4934e5460e3cb27d
330610 <jerry88tan@sbcglobal.net>
2016-01-20 09:38:08 -0800

    hu

--- app/src/main/java/com/example/csastudent2015/fitnessapp/BodyInfoFragment.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/BodyInfoFragment.java
@@ -1,9 +1,26 @@
 package com.example.csastudent2015.fitnessapp;
 
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by csastudent2015 on 1/12/16.
  */
 public class BodyInfoFragment extends Fragment{
+    private TextView mHeight;
+    private EditText mEnterHeight;
+    private TextView mWeight;
+    private EditText mEnterWeight;
+    private TextView mBodyInformation;
+
+    private Button mNext;
+
+
+
+
+
+
+
 }
