be63b8df3c5545df2e042c147d428297d5e6d299
chrischan53 <chrischan53@gmail.com>
2016-01-26 09:09:05 -0800

    exercise

--- app/src/main/java/com/example/csastudent2015/fitnessapp/ExerciseFragment.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/ExerciseFragment.java
@@ -6,11 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by csastudent2015 on 1/14/16.
  */
 public class ExerciseFragment extends Fragment {
+    private Spinner exerciseSpinner;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +22,16 @@ public class ExerciseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_exercise, container, false);
+        exerciseSpinner = (Spinner) rootView.findViewById(R.id.exercise_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.exercise_array, android.R.layout.simple_spinner_dropdown_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        exerciseSpinner.setAdapter(adapter);
         return rootView;
+
 
     }
 }
