0c19436195e152c6f2b354092ca079863303c9bf
csastudent2015 <jerrylshen@gmail.com>
2016-01-28 08:27:55 -0800

    notification emulator (http://www.tutorialspoint.com/android/android_notifications.htm)

--- app/src/main/java/com/example/csastudent2015/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/csastudent2015/fitnessapp/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.csastudent2015.fitnessapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notify("You've received new message", "");
+            }
+        });
     }
 
+    private void Notify(String notificationTitle, String notificationMessage){
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        @SuppressWarnings("deprecation")
+
+        Notification notification = new Notification(R.drawable.abc,"New Message", System.currentTimeMillis());
+        Intent notificationIntent = new Intent(this,NotificationView.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        notification.setLatestEventInfo(MainActivity.this, notificationTitle,notificationMessage, pendingIntent);
+        notificationManager.notify(9999, notification);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
